477b1cf1fd7ff0daa495b977bf07c7d0df48bb54
Lansing <me@lansing.io>
2020-05-19 21:58:39 -0400

    Add Jwt Authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Request, Get, UseGuards, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+    ) {}
 
   @Get()
   getHello(): string {
@@ -14,7 +18,7 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
   
 }
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,19 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './local.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, UsersService],
+      providers: [
+        AuthService,
+        UsersService,
+        JwtModule
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
-
+/*
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  */
 });
