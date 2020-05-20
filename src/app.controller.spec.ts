1e8acc2908c2e53aa228f04b45087bb29a594f10
Lansing <me@lansing.io>
2020-05-19 22:23:04 -0400

    Fixed the Test Files

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CatsModule } from './cats/cats.module';
 
 describe('AppController', () => {
   let app: TestingModule;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
+      imports: [CatsModule, AuthModule, UsersModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service';
 import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from './constants';
+import { UsersModule } from 'src/users/users.module';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        UsersService,
-        JwtModule
+      imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+          secret: jwtConstants.secret,
+          signOptions: { expiresIn: '60s' },
+        }),
       ],
+      providers: [AuthService, LocalStrategy, JwtStrategy]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
